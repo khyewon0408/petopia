fc54a3654639288feb5e746fd918ec2e756c7ebe
minju <minju@DESKTOP-KJRQJ1B>
2019-01-09 12:43:59 +0900

    190109_minju_join 수정중

--- Petopia/src/main/java/com/final2/petopia/controller/MemberController.java
+++ Petopia/src/main/java/com/final2/petopia/controller/MemberController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MemberController {
 	
-	@RequestMapping(value="/join.pet", method= {RequestMethod.GET})
+	@RequestMapping(value="/join.pet", method={RequestMethod.GET})
 	public String join() {
 		
 		return "join/join.tiles1";
+	} // end of public String join()
+	
+	@RequestMapping(value="/joinMember.pet", method= {RequestMethod.GET})
+	public String joinMember() {
+		
+		return "join/joinMember.tiles1";
 	} // end of public String join()
 	
 }
